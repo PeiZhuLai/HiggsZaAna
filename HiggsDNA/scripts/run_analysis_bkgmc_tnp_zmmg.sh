cc3d2c0151147664e1ecb20266cf5fbf18a1ae11
Pei-Zhu Lai <ht910155@gmail.com>
2026-03-14 16:46:34 +0100

    running job thing

--- HiggsDNA/scripts/run_analysis_bkgmc_tnp_zmmg.sh
+++ HiggsDNA/scripts/run_analysis_bkgmc_tnp_zmmg.sh
@@ -1,6 +1,6 @@
 outdir="/eos/home-p/pelai/HZa/parquet_tnp_zmmg/mc"
 
 # rm -fr /eos/home-p/pelai/HZa/parquet_tnp_zmmg/mc/job_1
-rm -fr /eos/home-p/pelai/HZa/parquet_tnp_zmmg/mc/analysis_manager.pkl
+# rm -fr /eos/home-p/pelai/HZa/parquet_tnp_zmmg/mc/analysis_manager.pkl
 
-python scripts/run_analysis.py --config "metadata/za_mc_tnp_zmmg_run3.json" --log-level "DEBUG" --n_cores 10 --output_dir $outdir --unretire_jobs --batch_system "local" --short #--with_skimmed  #--batch_system "local" condor
+python scripts/run_analysis.py --config "metadata/za_mc_tnp_zmmg_run3.json" --log-level "INFO" --n_cores 10 --output_dir $outdir --unretire_jobs --batch_system "condor" #--short #--with_skimmed DEBUG INFO #--batch_system "local" condor
