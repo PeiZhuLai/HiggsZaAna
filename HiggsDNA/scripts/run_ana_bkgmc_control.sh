9ad8c0663120ea0f1b45685df34c0ea530641f61
Pei-Zhu Lai <ht910155@gmail.com>
2026-04-27 08:08:50 +0200

    modify output name

--- HiggsDNA/scripts/run_ana_bkgmc_control.sh
+++ HiggsDNA/scripts/run_ana_bkgmc_control.sh
@@ -5,7 +5,7 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 repo_dir="$(cd "${script_dir}/.." && pwd)"
 cd "${repo_dir}"
 
-outdir="${OUTDIR:-/eos/home-p/pelai/HZa/parquet_DNA_control_tmp/Bkg_MC}"
+outdir="${OUTDIR:-/eos/home-p/pelai/HZa/parquet_DNA_control_tmp/Bkg_MC_control}"
 config="${CONFIG:-metadata/za_bkgmc_run3_control.json}"
 log_level="${LOG_LEVEL:-INFO}" # DEBUG # INFO
 n_cores="${N_CORES:-10}"
