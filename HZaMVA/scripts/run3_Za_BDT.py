0ba93f5b317dbae0d1a3170414aea25bfc5fcd31
Pei-Zhu Lai <ht910155@gmail.com>
2026-05-01 10:08:10 +0200

    open train

--- HZaMVA/scripts/run3_Za_BDT.py
+++ HZaMVA/scripts/run3_Za_BDT.py
@@ -650,7 +650,7 @@ def objective(trial):
 study_name = "Za-study"
 storage_name = "sqlite:///{}.db".format(study_name)
 xgb_study = optuna.create_study(directions=['maximize', 'minimize'], study_name=study_name, storage=storage_name, load_if_exists=True)
-# xgb_study.optimize(objective, n_trials=20, timeout=5000)
+xgb_study.optimize(objective, n_trials=20, timeout=5000)
 # xgb_study.optimize(objective, n_trials=5, timeout=300)
 
 # --- replace .show() with PDF export ---
