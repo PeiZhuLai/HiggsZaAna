191dd543269766dc5e970260fc06ad35c03630a5
Pei-Zhu, Lai <ht910155@gmail.com>
2026-05-21 00:07:42 +0800

    Force parquet merges in 3_merge_parquet

--- HiggsDNA/scripts/3_merge_parquet.sh
+++ HiggsDNA/scripts/3_merge_parquet.sh
@@ -4,16 +4,16 @@ scriptsDir="/afs/cern.ch/work/p/pelai/HZa/HiggsZaAna/HiggsDNA/scripts"
 
 # Nominal Samples
 
-PYTHONPATH=$PWD python $scriptsDir/A_merge_parquet_outputs.py /eos/project/h/htozg-dy-privatemc/pelai/HZa/parquet_DNA_tmp/Bkg_MC
+PYTHONPATH=$PWD python $scriptsDir/A_merge_parquet_outputs.py /eos/project/h/htozg-dy-privatemc/pelai/HZa/parquet_DNA_tmp/Bkg_MC --force
 
-PYTHONPATH=$PWD python $scriptsDir/A_merge_parquet_outputs.py /eos/project/h/htozg-dy-privatemc/pelai/HZa/parquet_DNA_tmp/Sig_MC
+PYTHONPATH=$PWD python $scriptsDir/A_merge_parquet_outputs.py /eos/project/h/htozg-dy-privatemc/pelai/HZa/parquet_DNA_tmp/Sig_MC --force
 
-PYTHONPATH=$PWD python $scriptsDir/A_merge_parquet_outputs.py /eos/project/h/htozg-dy-privatemc/pelai/HZa/parquet_DNA_tmp/Data
+PYTHONPATH=$PWD python $scriptsDir/A_merge_parquet_outputs.py /eos/project/h/htozg-dy-privatemc/pelai/HZa/parquet_DNA_tmp/Data --force
 
 # Control Samples
 
-PYTHONPATH=$PWD python $scriptsDir/A_merge_parquet_outputs.py /eos/project/h/htozg-dy-privatemc/pelai/HZa/parquet_DNA_control_tmp/Bkg_MC_control
+PYTHONPATH=$PWD python $scriptsDir/A_merge_parquet_outputs.py /eos/project/h/htozg-dy-privatemc/pelai/HZa/parquet_DNA_control_tmp/Bkg_MC_control --force
 
-PYTHONPATH=$PWD python $scriptsDir/A_merge_parquet_outputs.py /eos/project/h/htozg-dy-privatemc/pelai/HZa/parquet_DNA_control_tmp/Sig_MC_control
+PYTHONPATH=$PWD python $scriptsDir/A_merge_parquet_outputs.py /eos/project/h/htozg-dy-privatemc/pelai/HZa/parquet_DNA_control_tmp/Sig_MC_control --force
 
-PYTHONPATH=$PWD python $scriptsDir/A_merge_parquet_outputs.py /eos/project/h/htozg-dy-privatemc/pelai/HZa/parquet_DNA_control_tmp/Data_control
+PYTHONPATH=$PWD python $scriptsDir/A_merge_parquet_outputs.py /eos/project/h/htozg-dy-privatemc/pelai/HZa/parquet_DNA_control_tmp/Data_control --force
